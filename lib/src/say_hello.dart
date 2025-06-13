79981b5140a9b902324d485653c50e6ace907aaa
dev_fajar <dev_fajar@dev-fajars-MacBook-Air.local>
2025-06-13 23:37:15 +0700

    change say hello dart file

--- lib/src/say_hello.dart
+++ lib/src/say_hello.dart
@@ -1,4 +1,8 @@
 String sayHello(String name) {
   return 'Hello, $name!';
 }
+
+String hello(String name) {
+  return 'Hello, $name! Nice to meet you.';
+}
 // This function takes a name as input and returns a greeting message.
